d8f89a8b91ad57c8e31e8753d954338c642f28fb
= <=>
2018-09-30 16:27:02 -0300

    Adicionando parametros nos construtor

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,11 @@
+//Domínio da Negociação
+
+class Negociacao {
+
+    constructor(){
+        this.date = new Date(); //data atula
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+
+}
