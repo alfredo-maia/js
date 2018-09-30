3e4815484145e2a169b6f9ac666befa23653482b
= <=>
2018-09-30 16:33:05 -0300

    Passando parametros para o construtor

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -2,10 +2,10 @@
 
 class Negociacao {
 
-    constructor(){
-        this.date = new Date(); //data atula
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, quantidade, valor){
+        this.date = data; //data atula
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
 
 }
