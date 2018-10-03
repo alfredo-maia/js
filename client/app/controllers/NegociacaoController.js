a5e5bbee98df806ad776857943aa320c90c67d1a
alfredo-maia <alfredosouza100@gmail.com>
2018-10-03 20:23:31 -0300

    Associonado evento do formulário ao metódo do controller

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -4,6 +4,23 @@ class NegociacaoController {
         //Cancelando a submissão do formulário 
         event.preventDefault();
 
-        alert('Chamando a ação do controller');
+        let inputData = document.querySelector('#data');
+        let inputQuantidade = document.querySelector('#quantidade');
+        let inputValor = document.querySelector('#valor');
+
+        //Imprimindo valores afim de teste
+        console.log('Data: ' + inputData.value);
+        console.log('Quantidade: ' + inputQuantidade.value);
+        console.log('Valor: ' + inputValor.value);
+
+        //Especificando o tipo, o retorno é uma string
+        console.log(typeof(inputData.value));  
+        console.log(typeof(inputQuantidade.value));
+        console.log(typeof(inputValor.value));
+
+        //Convertendo para tipos especcificos
+        console.log(typeof(inputData.value));  
+        console.log(typeof(parseInt(inputQuantidade.value)));
+        console.log(typeof(parseFloat(inputValor.value)));
     }
 }
